6e5ebf2e8f4c998c3762de0663ff5a23afd93c7f
Anders Oscarsen <andersoscarsen@gmail.com>
2018-04-30 14:38:05 +0200

    2

--- radio.py
+++ radio.py
@@ -21,36 +21,47 @@ class Unit():
                 protocol.unit_codes[self.i - 1] |
                 (protocol.on_code if on else protocol.off_code))
 
-def weekday_filter(day, event):
-    pass
 
-class Schedule():
+day_names = {
+    'none':     set(),
+    'all':      set(range(7)),
+    'weekdays': set(range(5)),
+    'weekends': set(range(5,7)),
+}
+day_names_l = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday',
+    ]
+day_names.update((v, {k}) for k, v in enumerate(day_names_l))
+day_names.update((v, {k}) for k, v in enumerate(d + 's' for d in day_names_l))
 
-    def __init__(self, unit_name, on, off, days=('all',)):
+def which_days(days=('all',), not_days=None):
+    days_ = set()
+
+    for day_name in days or []:
+        days_ |= day_names[day_name]
+    for day_name in not_days or []:
+        days_ ^= day_names[day_name]
+
+    return days_
+
+class ScheduleEvent():
+
+    def __init__(self, unit_name, hour, minute, is_on=False, days=[]):
         self.unit_name = unit_name
 
-        hour_and_minute = lambda s: tuple(int(x) for x in s.split(':'))
-        self.on = [hour_and_minute(x) for x in on]
-        self.off = [hour_and_minute(x) for x in off]
-
-        day_names = {
-            'all':      list(range(7)),
-            'weekdays': list(range(5)),
-            'weekends': list(range(5,7)),
-        }
-        day_names.update((v, [k]) for k, v in enumerate([
-            'monday',
-            'tuesday',
-            'wednesday',
-            'thursday',
-            'friday',
-            'saturday',
-            'sunday',
-            ]))
-        days_ = set()
-        for day_name in days:
-            days_.update(day_names[day_name])
-        self.days = days_
+        # hour_and_minute = lambda s: tuple(int(x) for x in s.split(':'))
+        # self.on = hour_and_minute(on)
+        # self.off = hour_and_minute(off)
+        self.hour = hour
+        self.minute = minute
+        self.is_on = is_on
+        self.days = days
 
 
 class State():
@@ -73,26 +84,34 @@ class State():
                               protocol)
         self.units = units
 
-        events = collections.defaultdict(list)
-
+        events = {}
+        hour_and_minute = lambda s: tuple(int(x) for x in s.split(':'))
         for unit_event in d['schedule']:
+            unit_name = unit_event['unit']
 
-            for on_events in unit_event[True]:
-                pass
-            for off_events in unit_event[True]:
-                pass
+            for event in unit_event.get('events', []):
+                # def __init__(self, unit_name, on, off, days=('all',), not_days=None):
+                days = event[True]  # 'on' in yml == True
+                not_days = event.get('but_not_on', [])
+                if type(days) == str:
+                    days = [days]
+                if type(not_days) == str:
+                    not_days = [not_days]
+                ons = event.get('turn_on', [])
+                offs = event.get('turn_off', [])
+                if type(ons) == str:
+                    ons = [ons]
+                if type(offs) == str:
+                    offs = [offs]
+                for day in which_days(days, not_days):
+                    for on in ons:
+                        hour, minute = hour_and_minute(on)
+                        events[(day,hour,minute)] = (unit_name, 1)
+                    for off in offs:
+                        hour, minute = hour_and_minute(off)
+                        events[(day,hour,minute)] = (unit_name, 0)
 
         self.events = events
-
-        # schedule:
-          # - unit: bedroom_light
-            # on:
-              # - '07:00'
-              # - '21:00'
-            # off:
-              # - '10:00'
-              # - '22:00'
-            # days: weekdays
 
         self.timestamp = datetime.datetime.now()
 
